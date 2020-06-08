c5550a1c209c84e1547e1c7a772fafc4d489075b
Kevin Cochran <kcochran@hashicorp.com>
2020-06-07 22:21:44 -0400

    update

--- main.tf
+++ main.tf
@@ -3,31 +3,42 @@ provider "azurerm" {
 }
 
 module "vnet" {
-  source  = "app.terraform.io/kevindemos/custom-vnet/azure"
-  version = "1.0.0"
+    source  = "app.terraform.io/kevindemos/custom-vnet/azure"
+    version = "1.0.0"
 
-  identifier = var.identifier
-  location = var.location
+    identifier = var.identifier
+    location = var.location
+}
+
+module "custom_sg" {
+    source  = "app.terraform.io/kevindemos/custom-sg/azure"
+    version = "1.0.0"
+
+    res_group_name = module.vnet.res_group_name
+    identifier = var.identifier
+    location = var.location
+    subnet_id = module.vnet.subnet_id
+
 }
 
 module "blob" {
-  source  = "app.terraform.io/kevindemos/custom-blob/azure"
-  version = "1.0.0"
+    source  = "app.terraform.io/kevindemos/custom-blob/azure"
+    version = "1.0.0"
 
-  location = var.location
-  res_group_name = module.vnet.res_group_name
+    location = var.location
+    res_group_name = module.vnet.res_group_name
 }
 
 module "custom_vm" {
-  source  = "app.terraform.io/kevindemos/custom-vm/azure"
-  version = "1.0.1"
+    source  = "app.terraform.io/kevindemos/custom-vm/azure"
+    version = "1.0.2"
 
-  identifier = var.identifier
-  linux_password = var.linux_password
-  linux_user = var.linux_user
-  location = var.location
-  res_group_name = module.vnet.res_group_name
-  storage_endpoint = module.blob.storage_endpoint
-  subnet_id = module.vnet.subnet_id
-  vm_size = var.vm_size
+    identifier = var.identifier
+    linux_password = var.linux_password
+    linux_user = var.linux_user
+    location = var.location
+    res_group_name = module.vnet.res_group_name
+    storage_endpoint = module.blob.storage_endpoint
+    subnet_id = module.vnet.subnet_id
+    vm_size = var.vm_size
 }
