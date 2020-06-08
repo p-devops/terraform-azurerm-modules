615ccfe9a0a7fb9d893dbfaad6da715e72ffe5cd
Kevin Cochran <kcochran@hashicorp.com>
2020-06-07 22:06:31 -0400

    add variables

--- main.tf
+++ main.tf
@@ -22,12 +22,12 @@ module "custom_vm" {
   source  = "app.terraform.io/kevindemos/custom-vm/azure"
   version = "1.0.0"
 
-  identifier = "var.identifier"
-  linux_password = "var.linux_password"
-  linux_user = "var.linux_user"
-  location = "var.location"
-  res_group_name = "module.vnet.res_group_name"
-  storage_endpoint = "module.blob.storage_endpoint"
-  subnet_id = "var.subnet_id"
-  vm_size = "var.vm_size"
+  identifier = var.identifier
+  linux_password = var.linux_password
+  linux_user = var.linux_user
+  location = var.location
+  res_group_name = module.vnet.res_group_name
+  storage_endpoint = module.blob.storage_endpoint
+  subnet_id = var.subnet_id
+  vm_size = var.vm_size
 }
